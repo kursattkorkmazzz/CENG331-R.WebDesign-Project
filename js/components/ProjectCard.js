4b881f44b6f9c4e7e961b8a0533ba2bf56a003a0
gagir <agirgurkan06@gmail.com>
2024-05-09 15:01:44 +0300

    feat: adding project card to the list with form

--- js/components/ProjectCard.js
+++ js/components/ProjectCard.js
@@ -1,16 +1,18 @@
 class ProjectCard extends HTMLElement {
-  constructor() {
+  constructor(name = "Project Title", imagePath = "https://dummyimage.com/600x300/525252/fff.png") {
     super();
+    this.name = name;
+    this.imagePath = imagePath;
   }
   connectedCallback() {
     this.innerHTML = `
     <div id="card-wrapper">
     <img
       id="card-thumbnail"
-      src="https://dummyimage.com/600x300/525252/fff.png"
+      src=${this.imagePath}
     />
     <div id="card-bottom">
-      <h2 id="card-bottom-header">Project Title</h2>
+      <h2 id="card-bottom-header">${this.name}</h2>
       <button class="bg-blue text-white" onclick="window.location.assign('./project.html')">Go to Project</button>
     </div>
   </div>
